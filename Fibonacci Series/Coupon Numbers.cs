31ca7a12a2456a03224510f37b14556ec34173e8
Amit <amitkr24031995@gmail.com>
2021-09-24 16:24:41 +0530

    Added Coupon Number

---
+++ Fibonacci Series/Coupon Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Coupon_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            Random random = new Random();
+            //  Console.WriteLine(random.Next(chars.Length));
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Console.WriteLine("Coupon Number Generated : " + finalString);
+        }
+    }
+}
