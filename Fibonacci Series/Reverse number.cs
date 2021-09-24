5909714f9986761381e41786348e0d49348e65c5
Amit <amitkr24031995@gmail.com>
2021-09-24 16:18:59 +0530

    Adding using System;

---
+++ Fibonacci Series/Reverse number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, Reverse = 0, Rem;
+            Console.WriteLine("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            while (number != 0)
+            {
+                Rem = number % 10;
+                Reverse = Reverse * 10 + Rem;
+                number /= 10;
+            }
+            Console.WriteLine("Reversed Number : " + Reverse);
+            Console.ReadLine();
+        }
+    }
+}
