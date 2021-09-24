f1a9085d988ab7ce9ebdba5a1a8b6d03abc7d684
Amit <amitkr24031995@gmail.com>
2021-09-24 17:31:42 +0530

    Adding tempConversion

---
+++ Fibonacci Series/temperatureConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+
+            double celsius;
+            Console.Write("Enter the value of Celsius : ");
+
+            celsius = double.Parse(Console.ReadLine());
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+
+            Console.Write("Enter the value of Fehrenheit : ");
+            fahrenheit = double.Parse(Console.ReadLine());
+            Console.WriteLine("fahrenheit: " + fahrenheit);
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("celsius: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
