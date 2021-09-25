f0ed11897b056e21cd3f92a630b2dda720e5d154
Amit <amitkr24031995@gmail.com>
2021-09-25 17:54:13 +0530

    Adding binery Convertor

---
+++ Fibonacci Series/bineryConvertor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bineryConvetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter to Number : ");
+            int value = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine(binary);
+        }
+    }
+}
